2f1706efac9519ecec5469c349688b824b22e42a
Ильяс Герейханов <ilyas.gereykhanov@bk.ru>
2026-01-26 21:25:29 +0300

    fix: исправление логов парсера

--- parsers/parser/utils/s3_client.py
+++ parsers/parser/utils/s3_client.py
@@ -3,13 +3,15 @@ import boto3
 from botocore.client import Config
 from botocore.exceptions import ClientError
 from dotenv import load_dotenv
+from parser.core.logger import setup_logger
 
+logger = setup_logger()
 load_dotenv()
 
 ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY')
 SECRET_KEY = os.getenv('MINIO_SECRET_KEY')
-ENDPOINT = os.getenv('MINIO_ENDPOINT_URL', 'http://minio:9000')
-BUCKET_NAME = os.getenv('MINIO_BUCKET_NAME', 'raw-data')
+ENDPOINT = os.getenv('MINIO_ENDPOINT_URL')
+BUCKET_NAME = os.getenv('MINIO_BUCKET_NAME')
 
 s3_client = boto3.client(
     's3',
@@ -25,10 +27,10 @@ def upload_file_to_s3(file_path: str, object_name: str) -> bool:
             s3_client.head_bucket(Bucket=BUCKET_NAME)
         except ClientError:
             s3_client.create_bucket(Bucket=BUCKET_NAME)
-            print(f"Бакет {BUCKET_NAME} создан.")
+            logger.info(f"Бакет {BUCKET_NAME} создан.")
 
         s3_client.upload_file(file_path, BUCKET_NAME, object_name)
         return True
     except Exception as e:
-        print(f"Ошибка загрузки {file_path} в S3: {e}")
+        logger.info(f"Ошибка загрузки {file_path} в S3: {e}")
         return False
