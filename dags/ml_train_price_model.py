1c56c3bb8c689195897c12273c0c37ff532a9b10
Ильяс Герейханов <ilyas.gereykhanov@bk.ru>
2026-02-28 17:49:26 +0300

    fix: зафиксирован порядок строк в датасете для стабильности метрик

--- dags/ml_train_price_model.py
+++ dags/ml_train_price_model.py
@@ -76,18 +76,21 @@ def get_ml_dataset_from_pg_to_s3(**context):
                         CASE
                             WHEN metro_type = 'walk' THEN metro_min
                             ELSE metro_min * 5 -- примерно, умнножаем на 5 для трансопрта
-                        END as metro_min
+                        END as metro_min,
+                        flat_hash
                     from flats_db.gold.history_flats
                     where metro_min is not null
                 )
                 select
-                    * EXCLUDE (row_num),
+                    * EXCLUDE (row_num, flat_hash),
                     -- доп. признаки для модели 
                     (floor::DOUBLE / total_floors::DOUBLE) as rel_floor,
                     (area / (rooms_count + 1)) as area_per_room,
                     (total_floors > 18) as is_high_rise
                 from base_table
                 where row_num = 1
+                -- чтобы порядок в датасете был стабильный, иначе метрики скачут на тех же данных
+                order by flat_hash
             ) TO '{dataset_s3_key}' (FORMAT PARQUET);
             """
         )
